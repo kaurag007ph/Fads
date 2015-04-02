19b9a042d16f71791497b6de5c9bb93e91872e0d
Lawrence Marcelino <lawrence@migo.tv>
2015-04-02 13:55:06 +0800

    set public folder as absolute file

--- app/env/env.js
+++ app/env/env.js
@@ -11,7 +11,7 @@ var Env = {
         app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(methodOverride('X-HTTP-Method-Override'));
-        app.use(express.static(__dirname + '/public'));
+        app.use(express.static('./public'));
         app.listen(CONS.port, function() {
             console.log('Port running on: ' + CONS.port)
         });
